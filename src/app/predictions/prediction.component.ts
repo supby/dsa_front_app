90955c4a0dade3182bacc8ec8c36bc43d7d9f34b
Andre Suzanovich <supius@gmail.com>
2017-04-23 13:18:00 +0200

    add pipes for date and percent

--- src/app/predictions/prediction.component.ts
+++ src/app/predictions/prediction.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Component, Input, OnInit } from '@angular/core';
 import * as moment from 'moment';
 
 import { PredictionParams } from './prediction';
